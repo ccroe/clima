67ecd15f400c0c9d72290cab5f010239c88643bd
ccroe <romerochrystal35@gmail.com>
2024-06-18 18:18:40 -0700

    api key added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+function searchCity(city) {
+  let apiKey = "t134c4977ef73a2f9a6202bcb4ea1dob";
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
