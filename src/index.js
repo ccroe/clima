38d2f0e2d5ca222cbc6ad85240c30f839f6ec12f
ccroe <romerochrystal35@gmail.com>
2024-06-19 10:45:48 -0700

    weather id elements aded to index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector(".weather-app-temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condtion.description;
+  humidityElement.innerHTML = "${response.data.temperature.humidity}%";
+  windSpeedElement.innerHTML = "${response.data.wind.speed}km/h";
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML =
+    '<img src="${response.data.condition.icon_url}" class="weather-app-icon" />';
 }
 function searchCity(city) {
   let apiKey = "t134c4977ef73a2f9a6202bcb4ea1dob";
